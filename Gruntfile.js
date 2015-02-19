9df0db97ec404f7cb23e96e62dc1269559f1b54a
Scott Doxey <neogeek0101@gmail.com>
2015-02-18 22:45:07 -0500

    Added load-grunt-tasks package.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,6 @@
 module.exports = function (grunt) {
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-sass');
+    require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
 
